4ecf9b05a5a7d093eb2169f1a2e8efb476eb731c
juanpabloch <juanpablochoter@gmail.com>
2021-05-25 01:37:19 -0300

    mysql connection error5

--- database/mysqlConnect.js
+++ database/mysqlConnect.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql');
 const util = require('util');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: 'us-cdbr-east-03.cleardb.com',
     user: 'b1a7938b561099',
     password: '6d357f5a',
     database: 'heroku_e08c6defbbb5ac1'
 })
 
-connection.connect((error)=>{
-    if(error)throw new Error('No se pudo conectar con la base de datos');
-    console.log('conexion con la base de datos mysql establecida correctamente');
-})
+// connection.connect((error)=>{
+//     if(error)throw new Error('No se pudo conectar con la base de datos');
+//     console.log('conexion con la base de datos mysql establecida correctamente');
+// })
 
 const qy = util.promisify(connection.query).bind(connection);
 
